824f5d0a69e5ef9dbf43bc11be01f7dfdfa7ba2d
MrAzimzadeh <mehemmedezimzase@gmail.com>
2025-02-19 18:59:58 +0400

    add

---
+++ function/main.dart
@@ -0,0 +1,56 @@
+void main() {
+  printHelllo();
+}
+ 
+//  Basic Function
+void printHelllo() {
+  print('Helooooo');
+}
+
+/*
+// Parameterized Function
+void printName(String name) {
+  print('Hello $name');
+}
+// Function with return type
+int add(int a, int b) {
+  return a + b;
+}
+
+// Function with optional parameter
+void printNameOptional(String name, [int age = 10]) {
+  print('Hello $name');
+  if (age != null) {
+    print('Age: $age');
+  }
+}
+// Function with named parameter
+void printNameNamed({required String name, required int age}) {
+  print('Hello $name');
+  if (age != null) {
+    print('Age: $age');
+  }
+}
+// Function with default parameter
+void printNameDefault({String name = 'John', int age = 10}) {
+  print('Hello $name');
+  if (age != null) {
+    print('Age: $age');
+  }
+}
+// Function with optional positional parameter
+void printNamePositional(String name, [int age = 10]) {
+  print('Hello $name');
+  if (age != null) {
+    print('Age: $age');
+  }
+}
+// Lampda Function
+int addLamda(int a, int b) => a + b;
+// CallBack Function
+void printNameCallBack(String name, Function callBack) {
+  print('Hello $name');
+  callBack();
+}
+
+*/
---
+++ main.dart
@@ -0,0 +1,6 @@
+void main() {
+  var i = 0;
+  while (i <= 5) {
+    print(i++); // 1
+  }
+}
